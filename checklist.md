340fa450ed5639b84e6845a4a2916714b002d824
Marcus Noble <marcus@marcusnoble.co.uk>
2015-12-13 09:56:17 +0000

    BenWeek!

---
+++ checklist.md
@@ -0,0 +1,5 @@
+
+ - [ ] Wearing shorts
+ - [ ] Not wearing shoes
+ - [ ] Excessive use of the word "Yeah"
+ - [ ] [Over-exaggerating realisation](https://files.gitter.im/jsoxford/jsoxford.github.com/JSbn/Ben.gif)
---
+++ js/main.js
@@ -0,0 +1,18 @@
+var converter = new showdown.Converter({
+  tasklists: true,
+});
+window.fetch('checklist.md')
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(body) {
+    document.getElementById('checklist').innerHTML = converter.makeHtml(body);
+    var listItems = document.querySelectorAll('#checklist li');
+    for (var i = 0; i < listItems.length; i++) {
+      var element = listItems[i];
+      element.innerHTML = '<label>' + element.innerHTML + '</label>';
+      var checkbox = element.childNodes[0].childNodes[0];
+      checkbox.removeAttribute('disabled');
+
+    }
+  });
